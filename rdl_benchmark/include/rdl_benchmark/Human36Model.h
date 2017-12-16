9283c4648ba4068a505846aff828ece7c737dceb
Jordan Lack <jlack1987@gmail.com>
2017-12-16 16:24:32 +0000

    Revert "Trimmed the branch to only the rdl_dynamics sub directory"
    This reverts commit 5e7f7a6527a2b606f9630c418649b3b36121f753.

---
+++ rdl_benchmark/include/rdl_benchmark/Human36Model.h
@@ -0,0 +1,12 @@
+#ifndef _HUMAN36MODEL_H
+#define _HUMAN36MODEL_H
+
+namespace RobotDynamics
+{
+    class Model;
+}
+
+void generate_human36model(RobotDynamics::Model *model);
+
+/* _HUMAN36MODEL_H */
+#endif
